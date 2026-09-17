ed5fa4f68f4a95fd4135921d263337a6eb1cc7b3
mzereashte94 <mzereashte03@gmail.com>
2026-09-17 23:15:19 +0300

    Update FeatherApp.swift

--- Feather/FeatherApp.swift
+++ Feather/FeatherApp.swift
@@ -20,8 +20,10 @@ struct AshteMobileApp: App {
     @StateObject var downloadManager = DownloadManager.shared
     let storage = Storage.shared
     
+    // گۆڕاوەکە بۆ زانینی ئەوەی کە شاشەکە بینراوە یان نا
     @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding: Bool = false
     
+    // 💡 دروستکردنی بایندینگی سەلامەت بۆ ئەوەی Xcode ئیرۆر نەدات
     private var showOnboardingBinding: Binding<Bool> {
         Binding<Bool>(
             get: { !hasCompletedOnboarding },
@@ -39,8 +41,9 @@ struct AshteMobileApp: App {
                     .onOpenURL(perform: _handleURL)
                     .transition(.move(edge: .top).combined(with: .opacity))
             }
-            .animation(.easeInOut, value: downloadManager.manualDownloads.description)
+            .animation(.smooth, value: downloadManager.manualDownloads.description)
             
+            // 💡 بانگکردنی شاشەی خێرهاتنەکە بە سەلامەتی
             .fullScreenCover(isPresented: showOnboardingBinding) {
                 Group {
                     if #available(iOS 17.0, *) {
